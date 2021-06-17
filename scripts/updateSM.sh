4f6f018ea86ca35fdcfc7bde6127fc8130870229
Release Standard <releasestandard@netc.eu>
2021-06-17 18:49:08 +0200

    Submodule syncing

---
+++ scripts/updateSM.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+#
+# simple probleme:
+#  submodule of git repo don't track HEAD of their repo.
+#  so we have to update manually.
+# ex: buildSrc debug-logger.
+#
+# Related issue:
+#  https://stackoverflow.com/questions/8191299/update-a-submodule-to-the-latest-commit
+#
+op="$(pwd)";
+
+
+function init() {
+	echo "";
+	echo "$@";
+}
+function finit() {
+	echo "$@";
+	echo "";
+}
+
+
+
+# update top level submodules
+init "Syncing toplevel submodules ...";
+git submodule update --remote --merge
+
+# update nested submodules
+for module in $(git submodule | sed 's/^[ \t]*[^ \t]\+[ \t]*\([^ \t]\+\)[ \t]*.*/\1/g') ; do
+	cd "$module";
+	init "Updating modules of $module ..."
+	git submodule update --remote --merge
+	git add .
+	git commit -m "Submodule syncing"
+	git push --set-upstream origin master
+	finit
+	cd "$op";
+	git add "${module}"
+done
+
+# Commit top level submodules modifications
+git commit -m "Submodule syncing"
+git push --set-upstream origin master
+finit
