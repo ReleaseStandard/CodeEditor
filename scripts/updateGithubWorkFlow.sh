c52718c9ec5674e94c6736b20a0ad016d29f0326
Release Standard <releasestandard@netcourrier.com>
2021-07-29 15:40:13 +0000

    update workflow

--- scripts/updateGithubWorkFlow.sh
+++ scripts/updateGithubWorkFlow.sh
@@ -3,11 +3,16 @@
 # update workflows on github
 #
 
-
-git submodule foreach --recursive bash -c 'if ! [ "$(git status -s .github/)" = "" ] ; then \
-		cp ../.github/workflows/* ./.github/workflows/
-                git add .github/ ; \
-		git commit -m "update workflow" ; \
-                git push origin ; \
-        fi'
+rootModification=$(git status -s .github/)
+echo "rootModification=$rootModification"
+git submodule foreach --recursive bash -c "if ! [ \"$rootModification\" = \"\" ] ; then \
+		if [ -d ".github" ] ; then \
+			cp ../.github/workflows/* ./.github/workflows/ ; \
+			if ! [ \"\$(git status -s .github/)\" = \"\" ] ; then \
+		                git add .github/ ; \
+				git commit -m \"update workflow\" ; \
+		                git push origin ; \
+			fi ; \
+		fi ; \
+        fi"
 
