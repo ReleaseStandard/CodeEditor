48ed5089509e0f54b29cbbfc542a23eab20592d5
Release Standard <releasestandard@netcourrier.com>
2021-08-05 18:16:56 +0000

    Add test framework on gradle plugins

---
+++ scripts/testBuildSystem.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# That that every thing is working in the build system
+#
+#
+success=0
+failure=0
+function assert() {
+	if ( [ "$#" -lt "3" ] && [ "$?" -eq "0" ] ) || [ "$2" = "$3" ] ; then
+		printf "[\033[01;32mSUCCESS\033[0m] $1\n"
+		((success=success+1))
+		return 0
+	fi
+	echo "RESULTS"
+	echo " $2"
+	echo "EXPECTED"
+	echo " $3"
+	printf "[\033[01;31mFAILURE\033[0m] $1\n"
+	((failure=failure+1))
+	return 1
+}
+function init() {
+	echo "Launching tests ..."
+}
+function finit() {
+	if [ "$failure" -eq "0" ] ; then
+		echo "Tests done failure=${failure}, success=${success}"
+	else
+		echo "Tests failure failure=${failure}, success=${success}"
+	fi
+}
+
+init
+
+#
+## asserts
+#
+assert "project count 33" "$(./gradlew projects |grep -c 'Project')" "33"
+for mod in logger-debug editor widget-linenumber ; do
+	assert "build ${mod}" "$(./gradlew :CodeEditor-${mod}:assemble)"
+	op="$(pwd)"
+	cd "CodeEditor-${mod}"
+        assert "build from folder ${mod}" "$(./gradlew assemble)"
+	cd "$op"
+done
+
+finit
